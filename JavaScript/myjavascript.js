272b58c763c737ff9753e61c110515d7291bf20f
shreyasuthar655 <111381122+shreyasuthar655@users.noreply.github.com>
2023-04-02 23:34:21 +0530

    javascript added

---
+++ JavaScript/myjavascript.js
@@ -0,0 +1,28 @@
+    function checkk() {
+      var l = document.getElementById("Location").value;
+      var i = document.getElementById("Input").value;
+      city=["Anand","Ahmedabad","Vadodara","Surat"];
+      if (l == "") {
+        alert("Enter location");
+      }
+      if (!validateLoc(l)) {
+        alert("Please enter a valid Location.");
+        return;
+      }
+      function validateLoc(l) {
+        const re = /^[a-zA-Z]+$/;
+        return re.test(l);
+      }
+      if (!validateSearch(i)) {
+        alert("Please enter a valid Category.");
+        return;
+      }
+      function validateSearch(i) {
+        const re = /^[a-zA-Z0-9]+$/;
+        return re.test(i);
+      }
+      // for(var i=0;i<input.length;i++)
+      // {
+
+      // }
+    }
